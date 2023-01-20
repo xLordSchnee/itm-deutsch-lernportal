e6d93b6f32c4fafd35080650947e20956b51fecc
lisxsophie <lisasophieotto2000@gmail.com>
2023-01-20 10:10:36 +0100

    changes

--- components/base/navigation.vue
+++ components/base/navigation.vue
@@ -3,16 +3,6 @@
           expand-on-hover
           rail
         >
-          <v-list>
-            <v-list-item
-              prepend-avatar="https://randomuser.me/api/portraits/men/81.jpg"
-              title="Alexander Adams"
-              subtitle="alex.a88@gmail.com"
-            ></v-list-item>
-          </v-list>
-  
-          <v-divider></v-divider>
-  
           <v-list density="compact" nav>
             <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
             <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
