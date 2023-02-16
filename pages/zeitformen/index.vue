b769a1440644126dff534633f88674188e80b969
lisxsophie <lisasophieotto2000@gmail.com>
2023-02-16 21:46:49 +0100

    Anpassungen Zeitformen

--- pages/zeitformen/index.vue
+++ pages/zeitformen/index.vue
@@ -5,10 +5,225 @@
       <v-card-text class="Content">
         beschreibung
         <div class="video">
-          <ExerciseExternalLink url="https://www.google.com" title="google"></ExerciseExternalLink>
+          <ExerciseExternalLink url="https://bwvkrborde-my.sharepoint.com/personal/koll132756_students_bwv-ahaus_de/_layouts/15/stream.aspx?id=%2Fpersonal%2Fkoll132756_students_bwv-ahaus_de%2FDocuments%2FAnlagen%2Fsimpleshow_Zeitformen_in_der_deutschen_Sprache%2Emp4&ga=1" title="Zeitformen Video"></ExerciseExternalLink>
         </div>
       </v-card-text>
-    </v-card>
+      </v-card>
+      <div class="zeitform-content">
+        <div class="zeitform">
+            <h2> Zeit	Tempus	Beispiel</h2>
+            Gegenwart	Präsens	Du läufst den Berg hoch.
+            Vergangenheit	Präteritum
+            Perfekt
+            Plusquamperfekt	Sie lief den Berg hoch.
+            Wir sind den Berg hochgelaufen.
+            Wir waren den Berg hochgelaufen.
+            Zukunft	Futur I
+            Futur II	Du wirst den Berg hochlaufen.
+            Du wirst den Berg hochgelaufen sein.
+        </div>
+        <div class="zeitform">
+            <h2>Präsens:
+
+            Bildung:</h2>
+
+            <h3>Regelmäßige Verben</h3>
+            •	Verb + en
+            •	Beispiele: arbeiten > arbeite, machen > mache
+
+            <h3>Unregelmäßige Verben</h3>
+            •	Keine feste Endung
+            •	Beispiele: sein > bin, haben > habe, gehen > gehe
+            Modalverben
+            •	Verb (eigne Form)
+            •	Beispiele: müssen > muss, können > kann, wollen > will
+
+        </div>
+        <div class="zeitform">
+            <h2>Präteritum:
+
+            Bildung:</h2>
+            <h3>Regelmäßige Verben</h3>
+            •	Verb + -te –tet
+            •	Beispiel: arbeiten > arbeitete, machen > machte
+
+            <h3>Unregelmäßige Verben</h3>
+            •	Verb + keine feste Endung
+            •	Beispiele: sein > war, haben > hatte, gehen > ging
+
+            <h3>Modalverben</h3>
+            •	Verb (eigne Form)
+            •	müssen > musste, können > konnte, wollen > wollte
+
+        </div>
+        <div class="zeitform">
+            <h2>Perfekt (vollendete Gegenwartsform)</h2>
+            Die Zeitform Perfekt ist eine Vergangenheitsform, womit man Ereignisse beschreibt, die vor kurzem stattgefunden haben. obwohl Ereignisse im Perfekt bereits abgeschlossen sind, haben sie noch Auswirkungen auf die Gegenwart. Deswegen nennst du das Perfekt im Deutsch auch vollendete Gegenwart.
+            So sieht es in der Praxis aus:
+
+            o	Mit dem Perfekt wird über Ereignisse bzw. Handlungen gesprochen, die zwar abgeschlossen sind, dennoch aber auf die Gegenwart wirken.
+            Welcher Satz ist richtig?
+            1.	Gestern ging ich pünktlich zur Schule.
+            2.	Gestern bin ich pünktlich zur Schule gegangen. |Richtig|
+        </div>
+        <div class="zeitform">
+            <h2>Partizip II
+            Bildung:</h2>
+            •	Bei regelmäßigen (schwachen) Verben: ge + Verbstamm + (e)t
+            •	Kochen - Was hast du gekocht?
+            •	Bei unregelmäßigen (starken) Verben: ge + (veränderter) Verbstamm + en
+            •	Laufen - Er ist gelaufen.
+        </div>
+        <div class="zeitform">
+            <h2>Plusquamperfekt</h2>
+            (Vorvergangenheit / vollendete Vergangenheit)
+
+            Es geht um Ereignisse, welche vor einem vergangenen Ereignis stattgefunden haben.
+            Deswegen oft in Verbindung mit Präteritum oder Perfekt.
+
+            <h3>Bilden des Plusquamperfekts:</h3>
+            Haben oder sein (Präteritum) + Partizip II
+
+            <h3>Beispiel:</h3>
+            •	 Ich war mit dem Auto gefahren, bevor ich die Frau anfuhr.
+            •	Bevor ich iDeutsch Zeitformen
+            Zeit	Tempus	Beispiel
+            Gegenwart	Präsens	Du läufst den Berg hoch.
+            Vergangenheit	Präteritum
+            Perfekt
+            Plusquamperfekt	Sie lief den Berg hoch.
+            Wir sind den Berg hochgelaufen.
+            Wir waren den Berg hochgelaufen.
+            Zukunft	Futur I
+            Futur II	Du wirst den Berg hochlaufen.
+            Du wirst den Berg hochgelaufen sein.
+
+        </div>
+        <div class="zeitform">
+            <h2>Präsens:
+
+            Bildung:</h2>
+
+            <h3>Regelmäßige Verben</h3>
+            •	Verb + en
+            •	Beispiele: arbeiten > arbeite, machen > mache
+
+            <h3>Unregelmäßige Verben</h3>
+            •	Keine feste Endung
+            •	Beispiele: sein > bin, haben > habe, gehen > gehe
+            <h3>Modalverben</h3>
+            •	Verb (eigne Form)
+            •	Beispiele: müssen > muss, können > kann, wollen > will
+
+        </div>
+        <div class="zeitform">
+            <h2>Präteritum:
+
+            Bildung:</h2>
+            <h3>Regelmäßige Verben</h3>
+            •	Verb + -te –tet
+            •	Beispiel: arbeiten > arbeitete, machen > machte
+
+            <h3>Unregelmäßige Verben</h3>
+            •	Verb + keine feste Endung
+            •	Beispiele: sein > war, haben > hatte, gehen > ging
+
+            <h3>Modalverben</h3>
+            •	Verb (eigne Form)
+            •	müssen > musste, können > konnte, wollen > wollte
+        </div>
+        <div class="zeitform">
+            <h2>Perfekt (vollendete Gegenwartsform)</h2>
+            Die Zeitform Perfekt ist eine Vergangenheitsform, womit man Ereignisse beschreibt, die vor kurzem stattgefunden haben. obwohl Ereignisse im Perfekt bereits abgeschlossen sind, haben sie noch Auswirkungen auf die Gegenwart. Deswegen nennst du das Perfekt im Deutsch auch vollendete Gegenwart.
+            So sieht es in der Praxis aus:
+
+            •	Mit dem Perfekt wird über Ereignisse bzw. Handlungen gesprochen, die zwar abgeschlossen sind, dennoch aber auf die Gegenwart wirken.
+            Welcher Satz ist richtig?
+            1.	Gestern ging ich pünktlich zur Schule.
+            2.	Gestern bin ich pünktlich zur Schule gegangen. |Richtig|
+
+        </div>
+        <div class="zeitform">
+            <h2>Partizip II
+            Bildung:</h2>
+            •	Bei regelmäßigen (schwachen) Verben: ge + Verbstamm + (e)t
+            •	Kochen - Was hast du gekocht?
+            •	Bei unregelmäßigen (starken) Verben: ge + (veränderter) Verbstamm + en
+            •	Laufen - Er ist gelaufen.
+        </div>
+        <div class="zeitform">
+            <h2>Plusquamperfekt</h2>
+            (Vorvergangenheit / vollendete Vergangenheit)
+
+            Es geht um Ereignisse, welche vor einem vergangenen Ereignis stattgefunden haben.
+            Deswegen oft in Verbindung mit Präteritum oder Perfekt.
+
+            Bilden des Plusquamperfekts:
+            Haben oder sein (Präteritum) + Partizip II
+
+            <h3>Beispiel:</h3>
+            •	 Ich war mit dem Auto gefahren, bevor ich die Frau anfuhr.
+            •	Bevor ich ins Restaurant ging, hatte ich Geld abgehoben.
+
+        </div>
+        <div class="zeitform">
+            <h2>Futur I
+            (Einfache Zukunft)</h2>
+
+            Es können hiermit Aussagen über die Zukunft getroffen werden.
+
+            <h3>Bilden von Futur I:</h3>
+            Werden + Infinitiv (Grundform)
+
+            <h3>Beispiel:</h3>
+            •	Ich werde Brot kaufen.
+            •	Du wirst einen Kopfstand machen.
+
+        </div>
+        <div class="zeitform">
+            <h2>Futur II</h2>
+            (Vollendete Zukunft)
+
+            Es geht um Dinge, die in der Zukunft abgeschlossen sein werden.
+
+            <h3>Bilden von Futur II:</h3>
+            Werden + Partizip II + haben oder sein
+
+            <h3>Beispiel:</h3>
+            •	 Du wirst im Gefängnis gesessen haben.
+            •	Ich werde einen Computer gekauft haben.
+            ns Restaurant ging, hatte ich Geld abgehoben.
+        </div>
+        <div class="zeitform">
+            <h2>Futur I
+            (Einfache Zukunft)</h2>
+
+            Es können hiermit Aussagen über die Zukunft getroffen werden.
+
+            <h3>Bilden von Futur I:</h3>
+            Werden + Infinitiv (Grundform)
+
+            <h3>Beispiel:</h3>
+            •	Ich werde Brot kaufen.
+            •	Du wirst einen Kopfstand machen.
+        </div>
+        <div class="zeitform">
+
+            <h2>Futur II
+            (Vollendete Zukunft)</h2>
+
+            Es geht um Dinge, die in der Zukunft abgeschlossen sein werden.
+
+            <h3>Bilden von Futur II:</h3>
+            Werden + Partizip II + haben oder sein
+
+            <h3>Beispiel:</h3>
+            •	 Du wirst im Gefängnis gesessen haben.
+            •	Ich werde einen Computer gekauft haben.
+        </div>
+
+      </div>
+
 
     <!--    <img class="img_rick" src="assets/Rick.png" height="300px"/>-->
   </v-container>
