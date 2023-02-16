f952e70fdbd5e39d610e1ac256020f935cb80a2e
WlanKabl <55993320+PhilippStecher@users.noreply.github.com>
2023-02-16 16:17:49 +0100

    Work in progress

--- pages/das-dass/index.vue
+++ pages/das-dass/index.vue
@@ -1,11 +1,34 @@
 <template>
   <!-- Zeig task bei  -->
   <div class="container">
-    <h1>{{ Exercise.taskTitle }}</h1>
-    <h3>{{ Exercise.taskDescription }}</h3>
-
-    <SingleChoice :task="Exercise.tasks[0]"></SingleChoice>
-    <MultipleChoice></MultipleChoice>
+    <v-container class="mx-auto" style="max-width:750px;">
+    <v-card>
+      <v-card-title>{{ Exercise.taskTitle }}</v-card-title>
+      <v-card-text>{{ Exercise.taskDescription }}</v-card-text>
+      <v-card-text>
+        <!-- <div v-if="Exercise.tasks[page - 1].type == 0">
+          Single Choice
+        </div>
+        <div v-elseif="Exercise.tasks[page - 1].type == 1">
+          MultipleChoice
+        </div>
+        <div v-elseif="Exercise.tasks[page - 1].type == 2">
+          Gap Text
+        </div>
+        <div v-elseif="Exercise.tasks[page - 1].type == 3">
+          Gap Sorting
+        </div> -->
+        <SingleChoice :task="Exercise.tasks[page - 1]">
+          
+        </SingleChoice>
+      </v-card-text>
+    </v-card>
+    <v-pagination
+      v-model="page"
+      :length="Exercise.tasks.length"
+    ></v-pagination>
+<!--    <img class="img_rick" src="assets/Rick.png" height="300px"/>-->
+  </v-container>
   </div>
 
 
@@ -18,6 +41,11 @@ import MultipleChoice from "../../components/exercise/multiple-choice";
 import GapText from "../../components/exercise/gap-text";
 
 import Exercise from "../../data/task-templates/dasDass.json"
+
+const page = ref(1);
+const task = Exercise.task[page - 1];
+task.value = JSON.parse(task.value);
+// console.log(task.value);
 </script>
 
 <style scoped>
